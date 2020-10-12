7ee8287bd178d142f4620d42c2fb44ff69223037
Preston Haddock <quotablesarcasm@gmail.com>
2020-10-11 20:33:54 -0600

    Use env port instead of hard coded port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const Router = require('./routes/index');
 
 Router.route(app);
 
-app.listen(8080, () => {
-    console.log('Listening on port 3000');
+app.listen(process.env.DB_PORT, () => {
+    console.log(`Listening on Port ${process.env.DB_PORT}`);
 })
